5dc2012efe91815fb02e4c4741e1298cf3b28c59
romanthoruk2008-pixel <romanthoruk2008@gmail.com>
2026-05-10 15:26:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ import asyncio
 # ══════════════════════════════════════════
 # КОНФІГ — змінити тут
 # ══════════════════════════════════════════
-BOT_TOKEN = "8353488924:AAEQxCfIoCZEeSXiy8jVRXMb9Gf7-lJKnB4"
+BOT_TOKEN = "8353488924:AAGwuXyAKantDxiTyXAMb6DtLsA8_qzXbww"
 MINI_APP_URL = "https://paketatb.netlify.app/"
 
 # ══════════════════════════════════════════
